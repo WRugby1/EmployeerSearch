127d78fdb0d0103d39d5e094f81a457d92865f31
Tanner Austin <tanner.a.austin@gmail.com>
2020-11-04 11:32:32 -0600

    database changes

--- db/login.sql
+++
@@ -1,8 +0,0 @@
-USE jobHunt_db;
-
-CREATE TABLE login(
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    email VARCHAR(20),
-    password VARCHAR(25),
-    category VARCHAR(10),
-)
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,28 @@
+DROP DATABASE IF EXISTS jobHunt_db;
 CREATE DATABASE jobHunt_db;
 USE jobHunt_db;
+
+CREATE TABLE login(
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    email VARCHAR(20),
+    password VARCHAR(25),
+    category VARCHAR(10),
+);
+
+CREATE TABLE employer (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    company_name VARCHAR(25) NOT NULL,
+    job_title VARCHAR(25) NOT NULL,
+    industry VARCHAR(25) NOT NULL,
+    location VARCHAR(25) NOT NULL,
+    salary INTEGER(10)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCEREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(25),
+    job_title VARCHAR(25),
+    salary_ceiling INTEGER(10),
+    salary_floor INTEGER(10),
+    location VARCHAR(25)
+);

7fce4d95a201ec16437205a5acca29bc16a17480
Tanner Austin <tanner.a.austin@gmail.com>
2020-11-04 14:15:51 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
-console.log(config.use_env_variable)
+
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(config.use_env_variable, config);
